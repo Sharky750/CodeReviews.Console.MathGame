6b5a0de1f75a073ced446589d5be9ea21e8556cf
Akos Czirle <akos750@outlook.com>
2025-07-04 09:37:48 +0200

    Refresh

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,169 +1,175 @@
 ﻿
-    Console.WriteLine("Select the game");
-    Console.WriteLine("1. Osszeadas");
-    Console.WriteLine("2. Kivonas");
-    Console.WriteLine("3. Szorzas");
-    Console.WriteLine("4. Osztas");
-
-    string menuselect = Console.ReadLine();
-
-    int score = 0;
-
-switch (menuselect)
+internal class Program
+{
+    private static void Main(string[] args)
     {
+        Console.WriteLine("Select the game");
+        Console.WriteLine("1. Osszeadas");
+        Console.WriteLine("2. Kivonas");
+        Console.WriteLine("3. Szorzas");
+        Console.WriteLine("4. Osztas");
 
+        string menuselect = Console.ReadLine();
 
-        case "1":
-            OsszeadasGame();
-            break;
+        int score = 0;
 
-        case "2":
-            KivonasGame();
-            break;
-
-        case "3":
-            SzorzasGame();
-            break;
-
-        case "4":
-            OsszeadasGame();
-            break;
-
-
-    }
-
-
-
-    void OsszeadasGame()
-    {
-
-        
-
-        Random rnd = new Random();
-
-        int a = rnd.Next(1, 100);
-
-        int b = rnd.Next(1, 100);
-
-
-        Console.WriteLine($"Mennyi?-> {a} + {b} = ?");
-
-
-
-        int osszeadasAnswer = Convert.ToInt32(Console.ReadLine());
-
-        if (osszeadasAnswer == (a + b))
+        switch (menuselect)
         {
-            score++;
-            Console.WriteLine($"Fasza vagy more, tudsz számolni, az eredményed: {score}");
 
-        OsszeadasGame();        
 
-        }
-        else
-        {
-            Console.WriteLine($"Hát ez nem jött össze,  az eredményed: {score}");
+            case "1":
+                OsszeadasGame();
+                break;
+
+            case "2":
+                KivonasGame();
+                break;
+
+            case "3":
+                SzorzasGame();
+                break;
+
+            case "4":
+                OsszeadasGame();
+                break;
+
+
         }
 
 
 
-    }
-
-
-    void KivonasGame()
-    {
-
-        Random rnd = new Random();
-
-        int a = rnd.Next(1, 100);
-
-        int b = rnd.Next(1, 100);
-
-
-        Console.WriteLine($"Mennyi?-> {a} - {b} = ?");
-
-        int osszeadasAnswer = Convert.ToInt32(Console.ReadLine());
-
-        if (osszeadasAnswer == (a - b))
+        void OsszeadasGame()
         {
-        score++;
-        Console.WriteLine($"Fasza vagy more, tudsz számolni, az eredményed: {score}");
 
-        KivonasGame();
 
-    }
-        else
+
+            Random rnd = new Random();
+
+            int a = rnd.Next(1, 100);
+
+            int b = rnd.Next(1, 100);
+
+
+            Console.WriteLine($"Mennyi?-> {a} + {b} = ?");
+
+
+
+            int osszeadasAnswer = Convert.ToInt32(Console.ReadLine());
+
+            if (osszeadasAnswer == a + b)
+            {
+                score++;
+                Console.WriteLine($"Fasza vagy more, tudsz számolni, az eredményed: {score}");
+
+                OsszeadasGame();
+
+            }
+            else
+            {
+                Console.WriteLine($"Hát ez nem jött össze,  az eredményed: {score}");
+            }
+
+
+
+        }
+
+
+        void KivonasGame()
         {
-        Console.WriteLine($"Hát ez nem jött össze,  az eredményed: {score}");
-    }
+
+            Random rnd = new Random();
+
+            int a = rnd.Next(1, 100);
+
+            int b = rnd.Next(1, 100);
+
+
+            Console.WriteLine($"Mennyi?-> {a} - {b} = ?");
+
+            int osszeadasAnswer = Convert.ToInt32(Console.ReadLine());
+
+            if (osszeadasAnswer == a - b)
+            {
+                score++;
+                Console.WriteLine($"Fasza vagy more, tudsz számolni, az eredményed: {score}");
+
+                KivonasGame();
+
+            }
+            else
+            {
+                Console.WriteLine($"Hát ez nem jött össze,  az eredményed: {score}");
+            }
 
 
 
-    }
+        }
 
 
 
 
-    void SzorzasGame()
-    {
-
-        Random rnd = new Random();
-
-        int a = rnd.Next(1, 100);
-
-        int b = rnd.Next(1, 100);
-
-
-        Console.WriteLine($"Mennyi?-> {a} * {b} = ?");
-
-        int osszeadasAnswer = Convert.ToInt32(Console.ReadLine());
-
-        if (osszeadasAnswer == (a * b))
+        void SzorzasGame()
         {
-        score++;
-        Console.WriteLine($"Fasza vagy more, tudsz számolni, az eredményed: {score}");
 
-        SzorzasGame();
+            Random rnd = new Random();
 
-    }
-        else
+            int a = rnd.Next(1, 100);
+
+            int b = rnd.Next(1, 100);
+
+
+            Console.WriteLine($"Mennyi?-> {a} * {b} = ?");
+
+            int osszeadasAnswer = Convert.ToInt32(Console.ReadLine());
+
+            if (osszeadasAnswer == a * b)
+            {
+                score++;
+                Console.WriteLine($"Fasza vagy more, tudsz számolni, az eredményed: {score}");
+
+                SzorzasGame();
+
+            }
+            else
+            {
+                Console.WriteLine($"Hát ez nem jött össze,  az eredményed: {score}");
+            }
+
+
+
+        }
+
+
+
+        void OsztasGame()
         {
-        Console.WriteLine($"Hát ez nem jött össze,  az eredményed: {score}");
+
+            Random rnd = new Random();
+
+            int a = rnd.Next(1, 100);
+
+            int b = rnd.Next(1, 100);
+
+
+            Console.WriteLine($"Mennyi?-> {a} / {b} = ?");
+
+            int osszeadasAnswer = Convert.ToInt32(Console.ReadLine());
+
+            if (osszeadasAnswer == a / b)
+            {
+                score++;
+                Console.WriteLine($"Fasza vagy more, tudsz számolni, az eredményed: {score}");
+
+                OsztasGame();
+
+            }
+            else
+            {
+                Console.WriteLine($"Hát ez nem jött össze,  az eredményed: {score}");
+            }
+
+
+
+        }
     }
-
-
-
-    }
-
-
-
-    void OsztasGame()
-    {
-
-        Random rnd = new Random();
-
-        int a = rnd.Next(1, 100);
-
-        int b = rnd.Next(1, 100);
-
-
-        Console.WriteLine($"Mennyi?-> {a} / {b} = ?");
-
-        int osszeadasAnswer = Convert.ToInt32(Console.ReadLine());
-
-        if (osszeadasAnswer == (a / b))
-        {
-        score++;
-        Console.WriteLine($"Fasza vagy more, tudsz számolni, az eredményed: {score}");
-
-        OsztasGame();
-
-    }
-        else
-        {
-        Console.WriteLine($"Hát ez nem jött össze,  az eredményed: {score}");
-    }
-
-
-
-    }
+}
